e01a49b6d7c871b44b34495c65443a309aedae50
JanelRamirez <janel.2222107@gmail.com>
2023-01-03 11:15:18 -0400

    switches - templates

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,20 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+  nuevoJuego: string = '';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona: Persona = {
+    nombre: 'Janel',
+    favoritos: [
+      { id: 1, nombre: 'MLB 2k22' },
+      { id: 2, nombre: 'MLB 2k23' }
+    ]
   }
   guardar() {
     console.log('submit formulario');
 
   }
-
+  eliminar(index: number) {
+    this.persona.favoritos.splice(index, 1);
+  }
+  agragarJuego() {
+    const nuevoJuego: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({ ...nuevoJuego });
+    this.nuevoJuego = '';
+  }
 }
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -6,11 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  persona = {
+    genero: '',
+    notificaciones: false,
   }
-
+  terminosYCondiciones: boolean = false;
 }
